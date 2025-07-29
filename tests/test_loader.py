521cf9f97cd9407427902ce0a67a6ae364f6ff13
Sorrger <Sorrger@gmail.com>
2025-07-29 19:56:24 +0200

    Started implementing test_loader.py

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,93 +1,10 @@
+import pytest
 import pandas as pd
+from src.core.loader import load_csv, preview_dataframe, get_dataframe_stat_summary, set_column_names, validate_csv_format
 
-def load_csv(
-        filepath: str,
-        sep: str = ",",
-        header: int | None = 0,
-        encoding: str = "utf-8",
-        na_values: list[str] | None = None
-) -> pd.DataFrame:
-    """Load a CSV file into a pandas DataFrame.
-
-    Args:
-        filepath (str): _description_
-        sep (str, optional): _description_. Defaults to ",".
-        header (int | None, optional): _description_. Defaults to 0.
-        encoding (str, optional): _description_. Defaults to "utf-8".
-        na_values (list[str] | None, optional): _description_. Defaults to None.
-
-    Raises:
-        NotImplementedError: _description_
-
-    Returns:
-        pd.DataFrame: _description_
-    """
-    raise NotImplementedError()
-
-
-def preview_dataframe(
-        df: pd.DataFrame,
-        row_number,
-        tail: bool = False
-) -> pd.DataFrame:
-    """Return a preview of the DataFrame.
-
-    Args:
-        df (pd.DataFrame): _description_
-        row_number (_type_): _description_
-        tail (bool, optional): _description_. Defaults to False.
-
-    Raises:
-        NotImplementedError: _description_
-
-    Returns:
-        pd.DataFrame: _description_
-    """
-    raise NotImplementedError()
-
-
-def get_dataframe_stat_summary(
-        df: pd.DataFrame
-) -> pd.DataFrame:
-    """ Return summary statistics and structure of a DataFrame.
-
-    Args:
-        df (pd.DataFrame): _description_
-
-    Raises:
-        NotImplementedError: _description_
-
-    Returns:
-        pd.DataFrame: _description_
-    """
-    raise NotImplementedError()
-
-def set_column_names(df: pd.DataFrame, names: list[str]) -> pd.DataFrame:
-    """Manually set column names in a DataFrame.
-
-    Args:
-        df (pd.DataFrame): _description_
-        names (list[str]): _description_
-
-    Raises:
-        NotImplementedError: _description_
-
-    Returns:
-        pd.DataFrame: _description_
-    """
-    raise NotImplementedError()
-
-def validate_csv_format(filepath: str, expected_cols: int | None = None) -> bool:
-    """Check basic integrity of a CSV file before loading.
-
-    Args:
-        filepath (str): _description_
-        expected_cols (int | None, optional): _description_. Defaults to None.
-
-    Raises:
-        NotImplementedError: _description_
-
-    Returns:
-        bool: _description_
-    """
-    raise NotImplementedError()
+# def test_load_csv_valid_file(path):
+# def test_load_csv_missing_file(path):
+# def test_load_csv_invalid_separator(path, sep):
+# def test_load_csv_no_header(path):
+# def test_load_csv_custom_na_values(path):
+# def test_load_csv_encoding_utf16(path):
